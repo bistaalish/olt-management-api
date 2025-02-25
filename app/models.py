d6764ea0853c4e3caf1a89799019113648d872fe
Alish Bista <techchad2002@gmail.com>
2025-02-25 14:30:15 +0545

    Feat: added database model for services and device

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class Reseller(Base):
     email = Column(String, unique=True)
     phone = Column(String)
     users = relationship("User",  back_populates="reseller")
+    devices = relationship("Device", back_populates="reseller")
 
 class User(Base):
     __tablename__ = "users"
@@ -20,3 +21,29 @@ class User(Base):
     password = Column(String)
     reseller_id = Column(Integer, ForeignKey('resellers.id'))
     reseller = relationship("Reseller", back_populates="users")
+
+class Device(Base):
+    __tablename__ = "devices"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    vendor = Column(String)
+    model = Column(String)
+    type = Column(String)
+    ip = Column(String, unique=True)
+    username = Column(String)
+    password = Column(String)
+    reseller_id = Column(Integer, ForeignKey('resellers.id'))
+    reseller = relationship("Reseller", back_populates="devices")
+    serviceprofiles = relationship("ServiceProfile", back_populates="device")
+    
+class ServiceProfile(Base):
+    __tablename__ = "service_profiles"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    serviceprofile_id = Column(String)
+    lineprofile_id = Column(String)
+    gemport = Column(String)
+    vlan = Column(String)
+    device_id = Column(Integer, ForeignKey('devices.id'))
+    device = relationship("Device", back_populates="serviceprofiles")
