df52773fbe83102ae9cba7cc394d569d3803ad89
Alish Bista <techchad2002@gmail.com>
2025-02-26 07:42:09 +0545

    Feat: updated repository for user and resellers & removed useless shit

--- app/routers/reseller.py
+++ app/routers/reseller.py
@@ -32,35 +32,14 @@ def create(request: schemas.ResellerBase,db: Session= Depends(get_db)):
 @router.get("/{reseller_id}", status_code=status.HTTP_200_OK,response_model=schemas.ShowReseller)
 def get_reseller(reseller_id: int, response: Response , db: Session = Depends(get_db)):
     return reseller.getReseller(reseller_id,db)
-    # reseller = db.query(models.Reseller).filter(models.Reseller.id == reseller_id).first()
-    # if not reseller:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Reseller with id {reseller_id} not found")
-    # return reseller
+
 
 # DELETE /reseller/{reseller_id}
 @router.delete("/{reseller_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_reseller(reseller_id: int, db: Session = Depends(get_db)):
     return reseller.deleteReseller(reseller_id,db)
-    # if reseller_id == 1:
-    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Can't delete the Admin reseller")
-    # reseller = db.query(models.Reseller).filter(models.Reseller.id == reseller_id).delete(synchronize_session=False)
-    # if not reseller:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Reseller with id {reseller_id} not found")
-    # db.commit()
-    # return "deleted"  
 
 # PUT /reseller/{reseller_id}
 @router.put("/{reseller_id}", status_code=status.HTTP_202_ACCEPTED)
 def update_reseller(reseller_id: int, request: schemas.ResellerBase, db: Session = Depends(get_db)):
     return reseller.updateReseller(reseller_id,request,db)
-    # if reseller_id == 1:
-    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Can't update the Admin reseller")
-    # reseller = db.query(models.Reseller).filter(models.Reseller.id == reseller_id).first()
-    # if not reseller:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Reseller with id {reseller_id} not found")
-    # reseller.name = request.name
-    # reseller.email = request.email
-    # reseller.phone = request.phone
-    # db.commit()
-    # db.refresh(reseller)
-    # return "updated"
