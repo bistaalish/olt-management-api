ba4893fb87c82541778faa67f846f2c75245a148
Alish Bista <techchad2002@gmail.com>
2025-03-27 12:40:31 +0545

    Feat: Added HTTP logs

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, status, Response, HTTPException
+from fastapi import FastAPI, Depends, status, Response, HTTPException, Request
 from . import schemas, models, hashing
 # from typing import List
 from .database import engine,get_db,SessionLocal
@@ -7,10 +7,19 @@ from fastapi.middleware.cors import CORSMiddleware
 # from .hashing import Hash
 from .routers import reseller,user,auth,device,service
 import uvicorn
+import time
+import logging
 
 # Define allowed origins
 
 app = FastAPI()
+# Configure Logging
+logging.basicConfig(
+    filename="http_logs.log",  # Save logs to a file
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
 # Allow all origins (accessible from any IP)
 app.add_middleware(
     CORSMiddleware,
@@ -19,6 +28,33 @@ app.add_middleware(
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
 )
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+    
+    # Read request body (need to store as body can be read only once)
+    body = await request.body()
+    
+    # Log request details
+    logging.info(f"Request: {request.method} {request.url}")
+    logging.info(f"Headers: {dict(request.headers)}")
+    logging.info(f"Body: {body.decode('utf-8') if body else None}")
+
+    # Process request and get response
+    response = await call_next(request)
+    
+    # Get response body
+    response_body = b""
+    async for chunk in response.body_iterator:
+        response_body += chunk
+    response = Response(response_body, status_code=response.status_code, headers=dict(response.headers))
+
+    # Log response details
+    duration = time.time() - start_time
+    logging.info(f"Response: {response.status_code} | Duration: {duration:.4f}s")
+    logging.info(f"Response Body: {response_body.decode('utf-8') if response_body else None}\n")
+
+    return response
 models.Base.metadata.create_all(engine)
 
 # Include the auth router
