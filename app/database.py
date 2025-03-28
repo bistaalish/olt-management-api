62dead6eeda0dd77a481194d2d08f0ffbb4e968b
Alish Bista <techchad2002@gmail.com>
2025-03-28 12:40:11 +0545

    Feat: Added support for mysql

--- app/database.py
+++ app/database.py
@@ -1,12 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./flink.db"
+# Load environment variables
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False})
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./flink.db"
+# MySQL Database URL
+MYSQL_USER = str(os.getenv("MYSQL_USER"))
+MYSQL_PASSWORD = str(os.getenv("MYSQL_PASSWORD"))
+MYSQL_HOST = str(os.getenv("MYSQL_HOST", "localhost"))
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
+MYSQL_DATABASE = str(os.getenv("MYSQL_DATABASE"))
 
-SessionLocal = sessionmaker(bind=engine,autocommit=False,autoflush=False,)
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
+
+print(SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine,autocommit=False,autoflush=False)
 
 Base = declarative_base()
 
