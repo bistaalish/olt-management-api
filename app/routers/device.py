3dca2c0650191ea85b77d99f31d6a1d22d89d803
Alish Bista <techchad2002@gmail.com>
2025-05-11 00:32:56 +0545

    no clue

--- app/routers/device.py
+++ app/routers/device.py
@@ -131,6 +131,7 @@ def addONU(id,request:schemas.AddONU,db:Session = Depends(get_db),get_current_us
     UserInfo = db.query(models.User).filter(models.User.email == get_current_user.email).first()
     if get_current_user.roles == "Admin" or get_current_user.roles == "Support":
         AddONUOutput =  device.addONU(id,request,db,get_current_user.email)
+        print(AddONUOutput)
         data = AddONUOutput['data']
         if AddONUOutput["status"] == "failed":
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
