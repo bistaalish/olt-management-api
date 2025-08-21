5a5f06cbc22c129e56275c18d29f577c0f3946f4
Alish Bista <techchad2022@gmail.com>
2025-08-21 11:55:35 +0545

    Feat: changed autofind to telnet

--- app/repository/device.py
+++ app/repository/device.py
@@ -61,28 +61,28 @@ def updateDevice(device_id: int, request: schemas.Device, db: Session):
     db.refresh(device)
     return device
 
-#def findONU(id: int,db:Session):
-#    device = db.query(models.Device).filter(models.Device.id == id).first()
-#    if not device:
-#        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Device with id {id} not found")
-#    if device.vendor == "Huawei":
-#        tn = Huawei.TelnetSession(device)
-#        autofindResults = Huawei.autofind(tn)
-#        print(autofindResults["status"])
-#        if autofindResults["status"] == "failed":
-#            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No ONU Found on Autofind")
-#    return autofindResults['devices']
-
 def findONU(id: int,db:Session):
-    device = db.query(models.Device).filter(models.Device.id == id).first()
-    if not device:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Device with id {id} not found")
-    if device.vendor.lower() == "huawei":
-        autofindResults = HuaweiSNMP.RunAutofind(device)
-        print(autofindResults)
-        if autofindResults["status"] == "failed":
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=autofindResults["message"])
-        return autofindResults['data']
+   device = db.query(models.Device).filter(models.Device.id == id).first()
+   if not device:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Device with id {id} not found")
+   if device.vendor == "Huawei":
+       tn = Huawei.TelnetSession(device)
+       autofindResults = Huawei.autofind(tn)
+       print(autofindResults["status"])
+       if autofindResults["status"] == "failed":
+           raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No ONU Found on Autofind")
+   return autofindResults['devices']
+
+# def findONU(id: int,db:Session):
+#     device = db.query(models.Device).filter(models.Device.id == id).first()
+#     if not device:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Device with id {id} not found")
+#     if device.vendor.lower() == "huawei":
+#         autofindResults = HuaweiSNMP.RunAutofind(device)
+#         print(autofindResults)
+#         if autofindResults["status"] == "failed":
+#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=autofindResults["message"])
+#         return autofindResults['data']
 
 def SearchONU(id,request:schemas.ONUSearchSN,db: Session):
     device = db.query(models.Device).filter(models.Device.id == id).first()
